4949a00c9eda3c590a1d44a774b793cbd6669c88
David Downie <daviddownie@gmail.com>
2020-12-26 18:09:20 -0500

    added game logic and wins and losses board

---
+++ crystal-skull-collector/src/WinsAndLosses.js
@@ -0,0 +1,10 @@
+const WinsAndLosses = ({ wins, losses }) => {
+  return (
+    <div>
+      <p>Wins: {wins}</p>
+      <p>Losses: {losses}</p>
+    </div>
+  );
+};
+
+export default WinsAndLosses;
