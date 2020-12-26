c23aed3bd68d091508dda403100393a397f8a4fd
David Downie <daviddownie@gmail.com>
2020-12-26 14:59:01 -0500

    added initial state, and generating random target number

--- crystal-skull-collector/src/App.js
+++ crystal-skull-collector/src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
 
+import TargetNumber from "./TargetNumber";
+
 class App extends Component {
   // define initial state
+  state = {
+    skulls: [
+      { id: 1, assignedNumber: 0, src: "/assets/white-skull.jpg" },
+      { id: 2, assignedNumber: 0, src: "/assets/green-skull.jpg" },
+      { id: 3, assignedNumber: 0, src: "/assets/red-skull.jpg" },
+      { id: 4, assignedNumber: 0, src: "/assets/black-skull.jpg" },
+    ],
+    target: 0,
+    currentTotal: 0,
+    wins: 0,
+    losses: 0,
+  };
 
   // set the target number
-
+  setTarget() {
+    const currentTarget = Math.floor(Math.random() * 101) + 19;
+    this.setState({ target: currentTarget });
+  }
   // assign random number to each skull
 
   // when player clicks on skull
@@ -24,6 +41,8 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Crystal Skull Collector</h1>
+        <TargetNumber target={this.state.target} />
+        <button onClick={() => this.setTarget()}>Play</button>
       </div>
     );
   }
