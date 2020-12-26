2546e23956dc12ea3a6f1bbc87b23b9c387ca90d
David Downie <daviddownie@gmail.com>
2020-12-26 14:23:33 -0500

    Initial Commit

---
+++ crystal-skull-collector/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  // define initial state
+
+  // set the target number
+
+  // assign random number to each skull
+
+  // when player clicks on skull
+  // update the players current total for this game
+
+  // check if the players current total is equal or greater than the target number
+  // if the current total is lower than the target, continue with the game
+
+  // if the current total is equal
+  // increase wins
+  // reset the game
+  // if the current total is greater
+  // increase losses
+  // reset the game
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Crystal Skull Collector</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
