3bdc846c2c6469f4ab4021766a374afbb8556f11
David Downie <daviddownie@gmail.com>
2020-12-26 20:50:36 -0500

    added some general styles and added styles to rules section

---
+++ crystal-skull-collector/src/components/header/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Crystal Skull Collector</h1>
+    </header>
+  );
+};
+
+export default Header;
--- crystal-skull-collector/src/components/rules/Rules.js
+++ crystal-skull-collector/src/components/rules/Rules.js
@@ -3,15 +3,15 @@ import styles from "./Rules.module.css";
 const Rules = () => {
   return (
     <section className={styles.rules}>
-      <p>
+      <p className={styles.paragraph}>
         You will be given a random target number at the beginning of the game.
       </p>
-      <p>
+      <p className={styles.paragraph}>
         Each skull holds a point value that will be revealed to you upon
         clicking and then added to your point score. These points will change
         when you start a new game.
       </p>
-      <p>
+      <p className={styles.paragraph}>
         To win the game match the random number. If your score goes above the
         random number you lose the game.
       </p>
