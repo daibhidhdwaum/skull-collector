fec6ad3f08bbb6a57d78af94d29b32dd0f6dc4a4
David Downie <daviddownie@gmail.com>
2020-12-27 11:15:04 -0500

    styled skulls for mobile and desktop

--- crystal-skull-collector/src/components/winsAndLosses/WinsAndLosses.js
+++ crystal-skull-collector/src/components/winsAndLosses/WinsAndLosses.js
@@ -1,8 +1,10 @@
+import styles from "./winsAndLosses.module.css";
+
 const WinsAndLosses = ({ wins, losses }) => {
   return (
-    <div>
-      <p>Wins: {wins}</p>
-      <p>Losses: {losses}</p>
+    <div className={styles.container}>
+      <p className={styles.paragraph}>Wins: {wins}</p>
+      <p className={styles.paragraph}>Losses: {losses}</p>
     </div>
   );
 };
